07ab9e94a78ea5430e87f8774d0a6c3fac8554d3
aronmilenait <aronmilena.it@gmail.com>
2024-04-21 19:37:46 -0300

    SQLite database

--- app.py
+++ app.py
@@ -1,35 +1,58 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-tasks = []
+instance_path = os.path.join(app.root_path, 'instance')
+db_file_path = os.path.join(instance_path, 'tasks.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_file_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
+    tasks = Task.query.all()
     return render_template('index.html', tasks=tasks)
 
 @app.route('/tasks/add', methods=['POST'])
 def add_task():
-    task_title = request.form['task']  
-    new_task = {'id': len(tasks) + 1, 'task': task_title, 'done': False}
-    tasks.append(new_task)
+    task_title = request.form['task']
+
+    new_task = Task(task=task_title)
+    db.session.add(new_task)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
 @app.route('/tasks/<int:task_id>/edit', methods=['POST'])
 def edit_task(task_id):
-    updated_task = request.form['task']
-    for task in tasks:
-        if task['id'] == task_id:
-            task['task'] = updated_task
-            break
+    task_title = request.form['task']
+
+    task = Task.query.get(task_id)
+    if task:
+        task.task = task_title  # Update the task title
+        db.session.commit()
+
     return redirect(url_for('index'))
 
 @app.route('/tasks/<int:task_id>/delete', methods=['POST'])
 def delete_task(task_id):
-    for task in tasks:
-        if task['id'] == task_id:
-            tasks.remove(task)
-            break
+    task = Task.query.get(task_id)
+    if task:
+        db.session.delete(task)
+        db.session.commit()
+
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
